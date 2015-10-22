2a63f3836c853bc72cf28ce866b5a2fedca5c49b
xin_jin <xin_jin@wuhan.(none)>
2015-10-22 16:25:58 -0700

    add DSD_batch_yahoo.sh

--- DSD_batch_23features.sh
+++ DSD_batch_23features.sh
@@ -9,7 +9,6 @@ sed -i "s/#define F.*/#define F 11441 \/\/ d/g" src/SDSD_StructPlus4.c
 sed -i "s/#define F.*/#define F 22882 \/\/ d/g" src/SDSD_StructPlus5.c
 sed -i "s/#define F.*/#define F 45765 \/\/ d/g" src/SDSD_StructPlus6.c
 sed -i "s/#define F.*/#define F 100000 \/\/ d/g" src/SDSD_StructPlus7.c
-
 sed -i "s/#define F.*/#define F 44 \/\/ d/g" src/DSDS_StructPlus1.c
 sed -i "s/#define F.*/#define F 11441 \/\/ d/g" src/DSDS_StructPlus2.c
 sed -i "s/#define F.*/#define F 5720 \/\/ d/g" src/DSDS_StructPlus3.c
@@ -62,7 +61,6 @@ END
         sed -i "s/#define T.*/#define T 1310 \/\/ s/g" src/SDSD_StructPlus5.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDSD_StructPlus6.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDSD_StructPlus7.c
-
         sed -i "s/#define T.*/#define T 2 \/\/ s/g" src/DSDS_StructPlus1.c
         sed -i "s/#define T.*/#define T 5 \/\/ s/g" src/DSDS_StructPlus2.c
         sed -i "s/#define T.*/#define T 327 \/\/ s/g" src/DSDS_StructPlus3.c
@@ -70,12 +68,10 @@ END
         sed -i "s/#define T.*/#define T 1310 \/\/ s/g" src/DSDS_StructPlus5.c
         sed -i "s/#define T.*/#define T 2621 \/\/ s/g" src/DSDS_StructPlus6.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/DSDS_StructPlus7.c
-
         sed -i "s/#define T.*/#define T 5 \/\/ s/g" src/SDS_StructPlus1.c
         sed -i "s/#define T.*/#define T 655 \/\/ s/g" src/SDS_StructPlus2.c
         sed -i "s/#define T.*/#define T 2621 \/\/ s/g" src/SDS_StructPlus3.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDS_StructPlus4.c
-
 END
         LEAF=50
     fi
@@ -89,7 +85,6 @@ END
         sed -i "s/#define T.*/#define T 436 \/\/ s/g" src/SDSD_StructPlus5.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDSD_StructPlus6.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDSD_StructPlus7.c
-
         sed -i "s/#define T.*/#define T 1 \/\/ s/g" src/DSDS_StructPlus1.c
         sed -i "s/#define T.*/#define T 1 \/\/ s/g" src/DSDS_StructPlus2.c
         sed -i "s/#define T.*/#define T 109 \/\/ s/g" src/DSDS_StructPlus3.c
@@ -97,12 +92,10 @@ END
         sed -i "s/#define T.*/#define T 436 \/\/ s/g" src/DSDS_StructPlus5.c
         sed -i "s/#define T.*/#define T 873 \/\/ s/g" src/DSDS_StructPlus6.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/DSDS_StructPlus7.c
-
         sed -i "s/#define T.*/#define T 1 \/\/ s/g" src/SDS_StructPlus1.c
         sed -i "s/#define T.*/#define T 218 \/\/ s/g" src/SDS_StructPlus2.c
         sed -i "s/#define T.*/#define T 873 \/\/ s/g" src/SDS_StructPlus3.c
         sed -i "s/#define T.*/#define T 20000 \/\/ s/g" src/SDS_StructPlus4.c
-
 END
         LEAF=150
     fi
@@ -121,13 +114,11 @@ END
         echo 'DSDS'$VAR
         out/DSDS_StructPlus$VAR -ensemble /home/xin_jin/fast_rank/tree_model/MQ2007/tree-ensemble-file_20000_$LEAF -instances /home/xin_jin/fast_rank/input/MQ2007/100k.txt -maxLeaves $LEAF
     done
-
     for VAR in 1 2 3 4
     do
         echo 'SDS'$VAR
         out/SDS_StructPlus$VAR -ensemble /home/xin_jin/fast_rank/tree_model/MQ2007/tree-ensemble-file_20000_$LEAF -instances /home/xin_jin/fast_rank/input/MQ2007/100k.txt -maxLeaves $LEAF
     done
-
 END
     for VAR in 1 2 3 4
     do
@@ -136,7 +127,4 @@ END
     done
     echo
 done
-
-
-
 
