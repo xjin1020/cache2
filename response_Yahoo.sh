5658b88efce06e5921182f40ca3d94108acb019e
xin_jin <xin_jin@wuhan.(none)>
2015-10-23 11:27:02 -0700

    recompile

---
+++ response_Yahoo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Yahoo
+# Set F
+sed -i "s/#define F.*/#define F 373 \/\/ d/g" src/SDSD_StructPlus3.c
+
+# Set T
+for LOOP in 1 2 3
+do
+    if [ "$LOOP" = "1" ]; then
+       sed -i "s/#define T.*/#define T 1638 \/\/ s/g" src/SDSD_StructPlus3.c
+       LEAF=10
+    fi
+
+    if [ "$LOOP" = "2" ]; then
+        sed -i "s/#define T.*/#define T 327 \/\/ s/g" src/SDSD_StructPlus3.c
+        LEAF=50
+    fi
+
+    if [ "$LOOP" = "3" ]; then
+        sed -i "s/#define T.*/#define T 109 \/\/ s/g" src/SDSD_StructPlus3.c
+        LEAF=150
+    fi
+    
+    make
+    
+    echo "result of Leaf_num=$LEAF"
+    echo 'SDSD3'
+    out/SDSD_StructPlus3 -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_$LEAF/tree-ensemble-file_20000_$LEAF -instances /home/xin_jin/fast_rank/input/yahoo/1.txt -maxLeaves $LEAF
+    echo
+done
+
+
+
+
