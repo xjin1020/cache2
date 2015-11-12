0a0ed1cfb834fcef15742cbd06c7b61ef0fc2c62
xin_jin <xin_jin@wuhan.(none)>
2015-11-12 15:18:40 -0800

    add QS64bit

---
+++ sample_run_QS64bit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#out/QS -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_10/tree-ensemble-file_20000_10 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 10
+out/QS64bit -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_10000_50 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 50
