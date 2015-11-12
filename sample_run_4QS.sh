0ec8b0c70434acaa58b4e8c2a9e7c0d4b46ffc75
xin_jin <xin_jin@wuhan.(none)>
2015-11-12 12:57:49 -0800

    sample_run

--- sample_run_4QS.sh
+++ sample_run_4QS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #out/QS -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_10/tree-ensemble-file_20000_10 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 10
-out/QS -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_20000_150 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 150
+out/QS_4 -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_20000_150 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 150
--- sample_run_DOT.sh
+++ sample_run_DOT.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "out/DOT_StructPlus -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_150/tree-ensemble-file_1000_150 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 150"
-out/DOT_StructPlus -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_150/tree-ensemble-file_1000_150 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 150
+#out/DOT_StructPlus -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_15/tree-ensemble-file_20000_15 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 15
+out/DOT_StructPlus -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_10000_15 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 15
