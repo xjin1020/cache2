5e92224ecf01ddac07c6d47983cee0ac3a27f062
xin_jin <xin_jin@wuhan.(none)>
2015-11-16 12:19:06 -0800

    QS16bitopt.c

--- sample_run_QS.sh
+++ sample_run_QS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#out/QS -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_10/tree-ensemble-file_20000_10 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 10
-out/QS -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_20000_150 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 150
+#out/QS8bit -ensemble /home/xin_jin/fast_rank/tree_model/MSLR/tree-ensemble-file_1000_8 -instances /home/xin_jin/fast_rank/input/MSLR/1k.txt -maxLeaves 8
+out/QS16bitopt -ensemble /home/xin_jin/fast_rank/tree_model/yahoo/xin/leaf_16/tree-ensemble-file_1000_16 -instances /home/xin_jin/fast_rank/input/yahoo/1k.txt -maxLeaves 16
